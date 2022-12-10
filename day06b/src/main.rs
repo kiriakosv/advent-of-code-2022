d8f78a73428be62dd9d758f073defd1239cd605d
Kiriakos Velissariou <me@kiriakosv.xyz>
2022-12-10 11:05:47 +0200

    Add sixth day

---
+++ day06b/src/main.rs
@@ -0,0 +1,17 @@
+use std::collections::HashSet;
+
+fn main() {
+    const CONSECUTIVE: usize = 14;
+
+    let result = include_str!("../input.txt")
+        .trim_end_matches('\n')
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(CONSECUTIVE)
+        .into_iter()
+        .position(|candidate| candidate.iter().collect::<HashSet<_>>().len() == CONSECUTIVE)
+        .unwrap()
+        + CONSECUTIVE;
+
+    println!("{:?}", result);
+}
