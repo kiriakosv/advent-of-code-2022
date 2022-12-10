3d4e5263d7cbec92b85eb1068aac2ee004998a70
Kiriakos Velissariou <me@kiriakosv.xyz>
2022-12-10 10:57:10 +0200

    Improve day 5

--- day05a/src/main.rs
+++ day05a/src/main.rs
@@ -20,25 +20,22 @@ fn main() {
 
     let (_, s) = include_str!("../input.txt").split_once("\n\n").unwrap();
 
-    let instructions = s
-        .split_terminator('\n')
-        .map(|f| {
-            let l = f.split_terminator(' ').collect::<Vec<&str>>();
-            vec![l[1], l[3], l[5]]
-                .iter()
-                .map(|j| j.parse::<u32>().unwrap())
-                .collect::<Vec<u32>>()
-        })
-        .collect::<Vec<Vec<u32>>>();
+    let instructions = s.split_terminator('\n').map(|f| {
+        let l = f.split_terminator(' ').collect::<Vec<&str>>();
+        vec![l[1], l[3], l[5]]
+            .iter()
+            .map(|j| j.parse::<u32>().unwrap())
+            .collect::<Vec<u32>>()
+    });
 
-    instructions.iter().for_each(|instruction| {
-        let number_of_crates = *instruction.get(0).unwrap() as usize;
-        let from = (*instruction.get(1).unwrap() - 1) as usize;
-        let to = (*instruction.get(2).unwrap() - 1) as usize;
+    instructions.into_iter().for_each(|instruction| {
+        let number_of_crates = instruction[0] as usize;
+        let from = (instruction[1] - 1) as usize;
+        let to = (instruction[2] - 1) as usize;
 
-        let from_stack: &mut Vec<char> = &mut stacks[from];
+        let from_stack = &mut stacks[from];
         let to_be_moved = &mut from_stack.split_off(from_stack.len() - number_of_crates);
-        let to_stack: &mut Vec<char> = &mut stacks[to];
+        let to_stack = &mut stacks[to];
 
         to_be_moved.reverse();
         to_stack.append(to_be_moved);
