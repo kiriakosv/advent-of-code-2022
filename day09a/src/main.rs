5954c47b3a9887e87a9c8f5f4043c12bafff8fd7
Kiriakos Velissariou <me@kiriakosv.xyz>
2023-01-04 21:26:27 +0200

    wip

--- day09a/src/main.rs
+++ day09a/src/main.rs
@@ -1,6 +1,14 @@
 fn main() {
-    let motions = include_str!("../test_input.txt").split_terminator('\n')
-        .map(|line| line.split_once(' ').unwrap()).collect::<Vec<(&str, &str)>>();
+    let motions: Vec<&str> = include_str!("../test_input.txt")
+        .split_terminator('\n')
+        .fold(vec![], |mut motions, line| {
+            let (a, b) = line.split_once(' ').unwrap();
+            motions.push(vec![a; b.parse::<usize>().unwrap()]);
+            motions
+        })
+        .into_iter()
+        .flatten()
+        .collect();
 
     println!("{:?}", motions);
 }
